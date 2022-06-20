32d402991a905f6f8b586a77d6231f0f8aa0616e
Soran Najat Ibrahim Server Administrator <soribrsrvadm@bss-t-frz-bil01.ice.local>
2022-06-20 12:44:28 +0000

    db_test added

---
+++ db_test.py
@@ -0,0 +1,13 @@
+import psycopg2
+
+conn = psycopg2.connect(database="nrt_test", user="postgres", password="Nizam2007", host="127.0.0.1", port ="5432")
+
+cursor = conn.cursor()
+
+cursor.execute("select version()")
+
+data = cursor.fetchone()
+print("Connection established to: ", data)
+
+conn.cloase()
+print("CLOSED")
